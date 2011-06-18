023637909e1a9e0b4f06ff49badb64828a36554f
Ben Jones <ben.james.jones@gmail.com>
2011-06-18 13:42:18 -0700

    horizontal tested

--- jCollideTest.js
+++ jCollideTest.js
@@ -4,6 +4,8 @@ $(document).ready( function (){
 	$('<div><p>Hello</p></div>').css('border-style', 'solid').width(100).height(75).css('position', 'absolute').offset({left: 30, top: 30 + 100*i}).appendTo("body");
     }
     var elems = $.map($('div'), function(e){return new jcBody(e);});
+    elems[0].velocity.x = 500;
+
     jcLoop(elems, 
 	   jcGravity(new jcVec2(0, 100)));
 });
